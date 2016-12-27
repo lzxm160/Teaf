cb046cbc48f7a46795dde94cf3dac9f0a2f95df4
lzxm160 <lzxm160@sina.com>
2016-12-27 13:44:56 +0800

    first

--- trunk/teaf(isgw)/bin/start.sh
+++ trunk/teaf(isgw)/bin/start.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 if [ $# -lt 1 ]
 then
 {
@@ -8,7 +10,7 @@ fi
 
 SVRD_NAME=$1
 export ISGW_HOME=../
-export ISGW_BIN=${ISGW_HOME}/bin/
+export ISGW_BIN=${ISGW_HOME}/svr/
 export ISGW_CFG=${ISGW_HOME}/cfg/
 export LD_LIBRARY_PATH=./
 
@@ -16,9 +18,9 @@ export LD_LIBRARY_PATH=./
 export SYS_CONF_PATH=${ISGW_CFG}
 export SYS_SOCKET_TIMEOUT=2
 
-ulimit -c unlimited           #需要产生core文件
-ulimit -n 20480               #最大连接句柄数
-ulimit -s 2048                #每个线程的堆栈大小，如果单个消息很大需要调大此参数，比如20480
+ulimit -c unlimited           #产生core文件，不限制core文件大小
+ulimit -n 20480               #修改连接句柄数 
+ulimit -s 2048                #修改线程的堆栈大小，避免单个线程数使用内存太多
 
 #valgrind --tool=memcheck --leak-check=full --track-origins=yes --show-reachable=yes 
 ${ISGW_BIN}/${SVRD_NAME} 1>>${ISGW_BIN}/start.log 2>&1 &
